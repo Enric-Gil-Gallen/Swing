e479168d4d6f5037577f2d0b5b655df4dd0e091a
Enric-Gil-Gallen <Enric-Gil-Gallen@users.noreply.github.com>
2020-05-14 00:36:21 +0200

    Solucionado nombre

--- src/main/java/vista/InformaVista.java
+++ src/main/java/vista/InformaVista.java
@@ -1,14 +1,12 @@
 package vista;
 
-import modelo.datos.Factura;
-import modelo.datos.Llamada;
-import modelo.datos.Particular;
-import modelo.datos.Tarifa;
+import modelo.datos.*;
 
 public interface InformaVista {
     // Comunicarse directamente con el MODELO
 
     void nuevoPartucular(Particular particular);
+    void nuevoEmpresa(Empresa empresa);
     void nuevoTarifa(Tarifa tarifa);
     void nuevoFactura(Factura factura);
     void nuevaLlama(Llamada llamada);
