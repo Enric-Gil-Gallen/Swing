b9a5dd5bae41798e86ef650a1a6db5ee854ededf
ShadowStudioEnterprise <35380219+ShadowStudioEnterprise@users.noreply.github.com>
2020-05-15 12:27:46 +0200

    Set size

--- src/main/java/vista/ventanas/NewLLamadaJPanel.java
+++ src/main/java/vista/ventanas/NewLLamadaJPanel.java
@@ -5,10 +5,11 @@ import controlador.ImplementacionControlador;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class NewLLamadaJPanel extends JPanel {
-    JTextField nifT, numT, horainicioT, diaT;
-    JLabel nifL, importeL, fechaL,periodoL;
+public class NewLLamadaJPanel extends JPanel implements ActionListener {
+    JTextField nifT, numT, horainicioT, diaT, duracionT;
+    JLabel nifL, numL, horainicioL, diaL,duracionL;
     JButton button;
     private ImplementacionControlador controlador;
 
@@ -16,22 +17,27 @@ public class NewLLamadaJPanel extends JPanel {
         this.controlador = controlador;
         setPreferredSize( new Dimension( 1080, 720 ));
         button = new JButton("OK");
+        button.addActionListener(this);
         nifT = new JTextField();
         nifL = new JLabel("NIF/CIF");
         numT =new JTextField();
-        importeL = new JLabel("Importe");
+        numL = new JLabel("Número de telefono");
         horainicioT = new JTextField();
-        fechaL = new JLabel("Fecha");
+        horainicioL = new JLabel("hora inicip");
         diaT = new JTextField();
-        periodoL = new JLabel("Periodo");
+        diaL = new JLabel("Dia");
+        duracionL= new JLabel("Duración");
+        duracionT=new JTextField();
         add(nifL);
         add(nifT);
-        add(importeL);
+        add(numL);
         add(numT);
-        add(fechaL);
+        add(horainicioL);
         add(horainicioT);
-        add(periodoL);
+        add(diaL);
         add(diaT);
+        add(duracionL);
+        add(duracionT);
         add(button);
     }
 
@@ -39,6 +45,6 @@ public class NewLLamadaJPanel extends JPanel {
         //  Factura factura = new Particular(importT.getText(),/* clienteT.getText(),*/  Calendar.getInstance().getTime() /*,tarifaT.getText()*/, periodoT.getText(), tarifaT, direccionT.getText(), cpT.getText(), provinciaT.getText(), poblacionT.getText());
         //controlador.nuevaFactura(factura);
         JOptionPane.showMessageDialog(getParent(),
-                "Factura creada");
+                "Llamada creada");
     }
 }
