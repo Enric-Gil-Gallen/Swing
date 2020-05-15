9ee6d66790812d403c679663a7912542d6a66af5
ShadowStudioEnterprise <35380219+ShadowStudioEnterprise@users.noreply.github.com>
2020-05-15 13:23:45 +0200

    TarifaCreatorJPanel implementation

--- src/main/java/vista/ventanas/TarifaCreatorJPanel.java
+++ src/main/java/vista/ventanas/TarifaCreatorJPanel.java
@@ -2,14 +2,17 @@ package vista.ventanas;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TarifaCreatorJPanel extends JPanel {
+public class TarifaCreatorJPanel extends JPanel implements ActionListener {
     ButtonGroup group;
     JRadioButton basic, byhours, bydays;
-    JTextField importeL, periodo;
-    JButton save;
+    JTextField importeT, nombre, diaT, importeEspT, hInitT,hFinishT, importeET;
 
-    public TarifaCreatorJPanel() {
+    JButton save;
+    JPanel days, hour;
+    public TarifaCreatorJPanel(){
         setPreferredSize( new Dimension( 1080, 720 ));
         JPanel panel = new JPanel();
         group = new ButtonGroup();
@@ -22,12 +25,39 @@ public class TarifaCreatorJPanel extends JPanel {
         panel.add(basic);
         panel.add(byhours);
         panel.add(bydays);
+        add(panel);
         JPanel panelEdit=new JPanel();
-        periodo = new JTextField();//implementar
-        importeL=new JTextField();
-        panelEdit.add(importeL);
+        nombre = new JTextField();
+        nombre.setPreferredSize( new Dimension( 200, 24 ) );
+        nombre.add(nombre);
+        importeT =new JTextField();
+        importeT.setPreferredSize( new Dimension( 200, 24 ) );
+        panelEdit.add(importeT);
         save =new JButton("Guardar");
         panelEdit.add(save);
+        days =new JPanel();
+        diaT = new JTextField();
+        diaT.setPreferredSize( new Dimension( 200, 24 ) );
+        importeEspT = new JTextField();
+        importeEspT.setPreferredSize( new Dimension( 200, 24 ) );
+        days.add(diaT);
+        days.add(importeEspT);
+        hour =new JPanel();
+        hInitT= new JTextField();
+        hInitT.setPreferredSize( new Dimension( 200, 24 ) );
+        hFinishT = new JTextField();
+        hFinishT.setPreferredSize( new Dimension( 200, 24 ) );
+        importeET= new JTextField();
+        importeET.setPreferredSize( new Dimension( 200, 24 ) );
+        hour.add(hInitT);
+        hour.add(hFinishT);
+        hour.add(importeET);
+        
+        add(panelEdit);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
