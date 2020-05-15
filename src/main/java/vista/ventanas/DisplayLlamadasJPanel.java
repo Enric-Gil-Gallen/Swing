654f3353dbb5dc88b3f4202c51aa80d9b2100d5a
ShadowStudioEnterprise <35380219+ShadowStudioEnterprise@users.noreply.github.com>
2020-05-15 20:28:26 +0200

    Fix visual DisplayLlamadasJPanel

--- src/main/java/vista/ventanas/DisplayLlamadasJPanel.java
+++ src/main/java/vista/ventanas/DisplayLlamadasJPanel.java
@@ -15,24 +15,22 @@ public class DisplayLlamadasJPanel extends JPanel implements ActionListener {
     JPanel panelList;
     JLabel labelByID;
     JTextField textfield;
-    JButton all, byid;
+    JButton byid;
     JList<String> table;
     private ImplementacionModelo modelo;
     Vector<String> data;
 
-    public DisplayLlamadasJPanel(){
-        modelo = new ImplementacionModelo();
+    public DisplayLlamadasJPanel(ImplementacionModelo modelo){
+        this.modelo = modelo;
         setPreferredSize( new Dimension( 1080, 720 ));
         labelByID = new JLabel("Introduce el nif/cif del cliente");
         add(labelByID);
         textfield = new JTextField();
+        textfield.setPreferredSize(new Dimension(200, 24));
         add(textfield);
         byid = new JButton("Buscar");
         byid.addActionListener(this);
         add(byid);
-        all = new JButton("Obtener todos");
-        all.addActionListener(this);
-        add(all);
         panelList =new JPanel();
         add(panelList);
         data = new Vector<>();
